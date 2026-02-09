bb6bfe9e946993b14ad2c6c1a797bcca22ed2398
hokpy <rparkacev@gmail.com>
2026-02-09 20:45:30 +0500

    how to made if/else or ternar peration

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 const user = 'al'
+const isLoggedin = false
 
 export const App = () => {
+  let content = ''
+
+  if (isLoggedin) {
+    content = <p>`hi,${user}`</p>
+  } else {
+    content = <button>'log pleas'</button>
+  }
   return (
     <>
       <h1 className="header"></h1>
-      <p>hi,{user.toLowerCase()}</p>
-      {/* this is comment */}
-      <input id="email" type="email" required />
-      <label htmlFor="email">Email:</label>
-      <p style={{ color: 'red', fontWeight: 400 }}></p>
+      {isLoggedin && <p>hi,{user.toLowerCase()}</p>}
+      <p>{isLoggedin ? <p>`hi,${user}`</p> : <button>'log pleas'</button>}</p>
+      <p>{content}</p>
     </>
   )
 }
