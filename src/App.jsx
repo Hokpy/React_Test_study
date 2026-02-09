bb073adcdd20464cfff5d127a0989b8815cb58e7
hokpy <rparkacev@gmail.com>
2026-02-09 20:36:19 +0500

    feat: style page in jsx and remove all we don't need

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const user = 'al'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="header"></h1>
+      <p>hi,{user.toLowerCase()}</p>
+      {/* this is comment */}
+      <input id="email" type="email" required />
+      <label htmlFor="email">Email:</label>
+      <p style={{ color: 'red', fontWeight: 400 }}></p>
     </>
   )
 }
-
-export default App
